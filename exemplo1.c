cce6ac6a2677c8636dc746d8450f46e73ae62da0
guirut <ruotolo.costa@gmail.com>
2025-08-13 01:30:28 +0000

    Iclusão ano fundação FCI

--- exemplo1.c
+++ exemplo1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main () {
-	printf("Mackenzie - FCI\n");
+	printf("Mackenzie - FCI(1970)\n");
 	printf("Ciencia da computação\n")
 	printf("Algoritmos e Programação II\n"); 
 	return 0;

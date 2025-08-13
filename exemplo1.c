b368e8f0600f7882f643367fffe3689fd6e695a4
guirut <ruotolo.costa@gmail.com>
2025-08-13 01:29:43 +0000

    Inclusão nome do curso

--- exemplo1.c
+++ exemplo1.c
@@ -2,7 +2,7 @@
 
 int main () {
 	printf("Mackenzie - FCI\n");
-	printf("Algoritmos e "); 
-	printf("Prog. II\n");
+	printf("Ciencia da computação\n")
+	printf("Algoritmos e Programação II\n"); 
 	return 0;
 }

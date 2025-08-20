56ca33e6f9553c7303bcb9a0babbf454b41d66b5
guirut <ruotolo.costa@gmail.com>
2025-08-20 01:41:38 +0000

    Exercicio 5

--- Aula 2/ex5.c
+++ Aula 2/ex5.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
+int main(){
+    float r;
+    float area;
+
+    printf("Raio: \n");
+    scanf("%f", &r);
+
+    area = M_PI * (r * r);
+                        
+    printf("Um crirculo com raio %f tem área igual a %f\n", r, area);
+    
+    return 0;
+}
